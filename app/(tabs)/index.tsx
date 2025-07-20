089268201a1a90fd231e2f1d73ac00e8d9b43fa8
nico <nicodejager4@gmail.com>
2025-07-20 14:21:01 +0200

    Initial commit

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,28 +1,53 @@
-import { Text, View, StyleSheet } from "react-native";
-import {Link} from "expo-router";
+import * as ImagePicker from 'expo-image-picker';
+import { useState } from 'react';
+import { StyleSheet, View } from "react-native";
+import Button from "../components/button";
+import ImageViewer from "../components/imageViewer";
+
+const PlaceHolderImage = require('@/assets/images/background-image.png');
 
 export default function Index() {
+  const [selectedImage, setSelectedImage] = useState<string | undefined>(undefined);
+
+  const pickImageAsync = async () => {
+    let result = await ImagePicker.launchImageLibraryAsync({
+      mediaTypes: ['images'],
+      allowsEditing: true,
+      quality: 1
+    })
+    
+    if (!result.canceled) {
+      setSelectedImage(result.assets[0].uri);
+    } else {
+      alert('You did not select any image.')
+    }
+  }
   return (
     <View style={styles.container}>
-          <Text style={styles.text}>Home Screen</Text>
-          <Link href="/about" style={styles.button}>Go to About screen</Link>
+      <View style={styles.imageContainer}>
+        <ImageViewer imgSource={PlaceHolderImage} selectedImage={selectedImage}/>
+      </View>
+      <View style={styles.footerContainer}>
+        <Button theme='primary' label={"Choose a photo"} onPress={pickImageAsync} />
+        <Button label={"Use this photo"} />
+      </View>
     </View>
+
   );
 }
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: "#25292e",
-        alignItems: "center",
-        justifyContent: "center"
-    },
-    text: {
-        color: "#fff"
-    },
-    button: {
-        fontSize: 20,
-        textDecorationLine: 'underline',
-        color: '#fff',
-      },
+  container: {
+    flex: 1,
+    backgroundColor: "#25292e",
+    alignItems: "center",
+  },
+  imageContainer: {
+    flex: 1,
+    paddingTop: 28,
+  },
+  footerContainer: {
+    flex: 1 / 3,
+    alignItems: 'center'
+  }
 })
